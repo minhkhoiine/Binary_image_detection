b31c6fa2528c4dfd75a16a38166a8196e275ecd4
minhkhoiine <122579875+minhkhoiine@users.noreply.github.com>
2023-01-20 00:55:42 +0000

    Add files via upload

---
+++ TictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TictactoeApp.swift
+//  Tictactoe
+//
+//  Created by Khoi Vu on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
